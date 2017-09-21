d5feda294025f3d2e531d7fc96a8a463fcbd1af3
JasperWong <jasperwong48@gmail.com>
2017-09-21 21:29:31 +0800

    add transaction

--- src/main/java/com/jasper/framework/helper/AopHelper.java
+++ src/main/java/com/jasper/framework/helper/AopHelper.java
@@ -1,9 +1,11 @@
 package com.jasper.framework.helper;
 
 import com.jasper.framework.annotation.Aspect;
+import com.jasper.framework.annotation.Service;
 import com.jasper.framework.proxy.AspectProxy;
 import com.jasper.framework.proxy.Proxy;
 import com.jasper.framework.proxy.ProxyManager;
+import com.jasper.framework.proxy.TransactionProxy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -68,4 +70,28 @@ public final class AopHelper {
         }
         return targetMap;
     }
+
+    private static Map<Class<?>,Set<Class<?>>> createMap()throws Exception{
+        Map<Class<?>,Set<Class<?>>> proxyMap=new HashMap<>();
+        addAspectProxy(proxyMap);
+        addTransactionProxy(proxyMap);
+        return proxyMap;
+    }
+
+    private static void addAspectProxy(Map<Class<?>,Set<Class<?>>> proxyMap) throws Exception{
+        Set<Class<?>> proxyClassSet=ClassHelper.getClassSetBySuper(AspectProxy.class);
+        for(Class<?> proxyClass:proxyClassSet){
+            if(proxyClass.isAnnotationPresent(Aspect.class)){
+                Aspect aspect = proxyClass.getAnnotation(Aspect.class);
+                Set<Class<?>> targetClassSet=createTargetClassSet(aspect);
+                proxyMap.put(proxyClass,targetClassSet);
+            }
+        }
+    }
+
+    private static void addTransactionProxy(Map<Class<?>,Set<Class<?>>> proxyMap){
+        Set<Class<?>> serviceClassSet=ClassHelper.getClassSetByAnnotation(Service.class);
+        proxyMap.put(TransactionProxy.class,serviceClassSet);
+    }
+
 }
